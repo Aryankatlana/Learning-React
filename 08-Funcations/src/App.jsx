8fccff07f273e49cb278db0cd194130d0d38224c
Aryan08 <144986378+Aryankatlana@users.noreply.github.com>
2026-07-20 21:25:37 +0530

    Function in React

---
+++ 08-Funcations/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const App = () => {
+
+  function inputChanging(val){
+    console.log(val)
+  }   
+
+
+  return (
+    <div>
+     <input onChange={function(elem){
+      inputChanging(elem.target.value);
+     }} 
+     type="text" placeholder='Enter Name'/>
+    </div>
+  )
+}
+
+export default App
